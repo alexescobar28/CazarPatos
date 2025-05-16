b1e83fce9ce0e2f9aa0acc5e36c56790146da514
P3E009-E <P3E009-E@39681621D20P3>
2025-05-16 10:36:09 -0500

    Add Login Button

---
+++ app/src/main/java/com/Escobar/Alex/cazarpatos/LoginActivity.kt
@@ -0,0 +1,28 @@
+package com.Escobar.Alex.cazarpatos
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_login)
+        // Configurar el botón para ir a MainActivity
+        val loginButton = findViewById<Button>(R.id.buttonLogin)
+        loginButton.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
